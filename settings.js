52d96264ecf67ca7b43ed8eb8c8590114a2cf0df
cosmin <scosmin764@gmail.com>
2024-09-19 15:57:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "0770811929"
+global.ownername = "cosmin"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '0770811929'  //creator number
+global.ownername = '0770811929' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
